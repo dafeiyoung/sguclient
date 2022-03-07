f3978d786da6dbc214278f387ed266b94080c661
FurryAcetylCoA <1109673069@qq.com>
2022-03-07 13:14:37 +0800

    大幅度修改Luci面板，增加日志输出功能。同时修正init.d中不正确的写法

--- sguclient-OpenWrt-LuCI/sguclient/luci_files/root/usr/lib/lua/luci/model/cbi/sguclient/sguclient.lua
+++ sguclient-OpenWrt-LuCI/sguclient/luci_files/root/usr/lib/lua/luci/model/cbi/sguclient/sguclient.lua
@@ -21,27 +21,62 @@ s.anonymous = true
 enable = s:option(Flag, "enable", translate("Enable"),translate("Main control of SGUClient"))
 enable = s:option(Flag, "autoreconnect", translate("Auto Reconnect"),translate("Reconnect if client went off-line(Generally NOT checked)"))
 enable = s:option(Flag, "noheartbeat", translate("No 1x Heart Beat"),translate("No 802.1x heart beat and cancel alarm(Generally NOT checked)"))
+
 name = s:option(Value, "username", translate("1x Username"),translate("Fill in your 802.1x username"))
 pass = s:option(Value, "password", translate("1x Password"),translate("Fill in your 802.1x password"))
 pass.password = true
 
 isptype=s:option(ListValue,"isptype",translate("ISP Type"),translate("Chose your ISP Type"))
 isptype:value("D",translate("CTCC(DX)"))
-isptype:value("Y",translate("CMCC(YD)")) 
+isptype:value("Y",translate("CMCC(YD)"))
 
 ifname = s:option(ListValue, "ifname", translate("AuthInterface"),translate("Chose your authentication interface"))
-
-wanip = s:option(Value, "wanip", translate("Wan IP Address"),translate("Authentication interface IPv4 address(must be the same as which filled in 'Network-Interfaces')"))
-wanip.datatype="ip4addr"
-
-for k, v in ipairs(luci.sys.net.devices()) do
+for k, v in pairs(luci.sys.net.devices()) do
 	if v ~= "lo" then
 		ifname:value(v)
 	end
 end
 
+
+
+function ifname.validate(self, value) --未保存时无法直接传递选择的网卡名 通过文件中转
+	luci.sys.exec("touch /tmp/sguclient.ifname")
+	luci.sys.exec("echo %s >/tmp/sguclient.ifname"%value)
+	return value
+end
+
+
+b=s:option(Button,"GetIpFromIf",translate("Get Ip Address from interface"))
+
+wanip = s:option(Value, "wanip", translate("Wan IP Address"),translate("Authentication interface IPv4 address(must be the same as which filled in 'Network-Interfaces')"))
+wanip.datatype="ip4addr"
+
+
+function b.write(self,section,value)
+	local ifip
+	local readdd=luci.sys.exec(" tail -1 /tmp/sguclient.ifname  ")
+	if(readdd~="" and #readdd <=16) then --后面那个是防止找不到文件
+		readdd = (string.gsub(readdd, "'" , ""))
+		readdd = (string.gsub(readdd, "\n", ""))
+		readdd = (string.gsub(readdd, "\r", ""))
+
+		for _, v in pairs(nixio.getifaddrs()) do
+			if v.family == "inet" and v.name == readdd then
+				ifip=v.addr
+			end
+		end
+	end
+
+	function wanip.cfgvalue()  -- 从mentohust看到的奇妙用法
+		return ifip or ""
+	end
+end
+
+
+
 local apply = luci.http.formvalue("cbi.apply")
 if apply then
+	nixio.fs.remove("/tmp/sguclient.ifname")
 	io.popen("/etc/init.d/sguclient restart")
 end
 
