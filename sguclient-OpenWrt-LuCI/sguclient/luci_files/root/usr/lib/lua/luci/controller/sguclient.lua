1b3e9912bc99b375685dd082f95b4c2a18194f63
FurryAcetylCoA <1109673069@qq.com>
2022-03-07 13:14:37 +0800

    大幅度修改Luci面板，增加日志输出功能。同时修正init.d中不正确的写法

--- sguclient-OpenWrt-LuCI/sguclient/luci_files/root/usr/lib/lua/luci/controller/sguclient.lua
+++ sguclient-OpenWrt-LuCI/sguclient/luci_files/root/usr/lib/lua/luci/controller/sguclient.lua
@@ -1,5 +1,19 @@
 module("luci.controller.sguclient", package.seeall)
 
 function index()
-        entry({"admin", "network", "sguclient"}, cbi("sguclient"), _("SGUClient LuCI"), 100)
+        if not nixio.fs.access("/etc/config/sguclient")  then
+            call("act_reset")
         end
+        local page
+
+        page =entry({"admin", "network", "sguclient"}, firstchild(), _("SGUClient LuCI"), 80)
+
+        entry({"admin", "network", "sguclient","client"}, cbi("sguclient/sguclient"), _("SGUClient LuCI"), 1)
+
+
+        entry({"admin", "network", "sguclient","Log"}, cbi("sguclient/log"), _("Log"), 2).leaf =true
+end
+
+function act_reset()
+        --Not impl yet
+end
