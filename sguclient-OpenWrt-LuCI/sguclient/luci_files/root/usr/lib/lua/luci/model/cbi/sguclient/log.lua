aafdf7b1dc9b81a1c0502d9d7fcdb5f80d4a5163
FurryAcetylCoA <1109673069@qq.com>
2022-03-10 08:50:06 +0800

    大幅度修改Luci面板，增加日志输出功能。同时修正init.d中不正确的写法

---
+++ sguclient-OpenWrt-LuCI/sguclient/luci_files/root/usr/lib/lua/luci/model/cbi/sguclient/log.lua
@@ -0,0 +1,17 @@
+require("luci.sys")
+
+m = Map("sguclient", translate("SGUClient Log"), translate("Log file:/tmp/sguclient.log"))
+
+s = m:section(TypedSection, "login", "")  --这里和上面的map一定要能对应到一个已经存在的配置文件的配置字段
+s.addremove = false
+s.anonymous = true
+
+view_cfg = s:option(TextValue, "1", nil)
+    view_cfg.rows = 25
+    view_cfg.readonly = true
+  
+    function view_cfg.cfgvalue()  
+        return nixio.fs.readfile("/tmp/sguclient.log") or ""  
+    end  
+
+return m
