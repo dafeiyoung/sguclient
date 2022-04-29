6b383e9e0afa50b89aed0ea19d50c5ae2ef4a9af
IDeLoveYou <57473616+IDeLoveYou@users.noreply.github.com>
2022-04-29 20:39:10 +0800

    改进了luci界面，更加人性化的操作
    删除一些繁杂的过渡文件，优化操作

--- sguclient-OpenWrt-LuCI/sguclient/luci_files/root/usr/lib/lua/luci/model/cbi/sguclient/sguclient.lua
+++ sguclient-OpenWrt-LuCI/sguclient/luci_files/root/usr/lib/lua/luci/model/cbi/sguclient/sguclient.lua
@@ -31,51 +31,15 @@ isptype:value("D", translate("CTCC(DX)"))
 isptype:value("Y", translate("CMCC(YD)"))
 
 ifname = s:option(ListValue, "ifname", translate("AuthInterface"), translate("Chose your authentication interface"))
-for k, v in pairs(luci.sys.net.devices()) do
-    if v ~= "lo" then
-        ifname:value(v)
-    end
-end
-
-function ifname.validate(self, value)
-    --未保存时无法直接传递选择的网卡名 通过文件中转
-    luci.sys.exec("touch /tmp/sguclient.ifname")
-    luci.sys.exec("echo %s >/tmp/sguclient.ifname" % value)
-    return value
-end
-
-b = s:option(Button, "GetIpFromIf", translate("Get Ip Address from interface"))
-b.inputtitle = translate("Click Refresh to get")
-
-wanip = s:option(DummyValue, "wanip", translate("Wan IP Address"), translate("Authentication interface IPv4 address(view only)"))  --自从不允许输入IP后 这里就这样了
-
-
-function b.write(self, section, value)
-    local ifip
-    local readdd = luci.sys.exec(" tail -1 /tmp/sguclient.ifname  ")
-    if (readdd ~= "" and #readdd <= 16) then
-        --后面那个是防止找不到文件
-        readdd = (string.gsub(readdd, "'", ""))
-        readdd = (string.gsub(readdd, "\n", ""))
-        readdd = (string.gsub(readdd, "\r", ""))
-
+for _, network in pairs(luci.sys.net.devices()) do
+    if network ~= "lo" then
         for _, v in pairs(nixio.getifaddrs()) do
-            if v.family == "inet" and v.name == readdd then
+            if v.family == "inet" and v.name == network then
                 ifip = v.addr
+                ifname:value(network, translate("%s (%s)" %{ network, ifip }))
             end
         end
     end
-
-    function wanip.cfgvalue()
-        -- 从mentohust看到的奇妙用法
-        return ifip or ""
-    end
-end
-
-local apply = luci.http.formvalue("cbi.apply")
-if apply then
-    nixio.fs.remove("/tmp/sguclient.ifname")  --也许可以吧这个持久化一下,让每次打开设置页面都能显示ip?
-   -- io.popen("/etc/init.d/sguclient restart")
 end
 
 return m
