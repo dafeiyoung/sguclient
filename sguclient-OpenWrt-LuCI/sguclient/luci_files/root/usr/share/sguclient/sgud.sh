cd50fbdb39787d83354c9291a3df84a0dcb8d28a
FurryAcetylCoA <68182718+FurryAcetylCoA@users.noreply.github.com>
2022-05-12 22:40:18 +0800

    修正sgud.sh使其可以正常运行在ash上
    错误的合并重定向符号 
    ShellCheck：SC2094、SC2069、SC2155、SC2046、SC3010、SC2068等

--- sguclient-OpenWrt-LuCI/sguclient/luci_files/root/usr/share/sguclient/sgud.sh
+++ sguclient-OpenWrt-LuCI/sguclient/luci_files/root/usr/share/sguclient/sgud.sh
@@ -5,9 +5,10 @@ LOG_FILE=/var/log/sguclient.log
 
 clean_log() {
 
-  local logsnum=$(cat $LOG_FILE 2>/dev/null | wc -l)
-  [ "$logsnum" -gt 300 ] && {
-    tail -n 10 $LOG_FILE >$LOG_FILE
+  local logsnum
+  logsnum=$(cat $LOG_FILE 2>/dev/null | wc -l)
+  [ "$logsnum" -gt 200 ] && {
+    tail -n 10 $LOG_FILE > ${LOG_FILE}.tmp && mv ${LOG_FILE}.tmp $LOG_FILE
     echo "$(date "+%Y-%m-%d %H:%M:%S") 日志文件过长，清空处理！" >>$LOG_FILE
   }
 
@@ -20,23 +21,23 @@ if [ -f "/var/run/sgud.sh.pid" ]; then
   echo "pid文件存在"
 else
   echo "pid文件不存在，手动创建pid"
-	touch /var/run/sgud.sh.pid 2&>1 >/dev/null
+	touch /var/run/sgud.sh.pid  >/dev/null 2>&1
 	pgrep sgud.sh -f >/var/run/sgud.sh.pid 2>&1 &
 fi
 
 # 首次启动sguclient
-/bin/sguclient $@ 2&>1 1>>$LOG_FILE &
-
+/bin/sguclient "$@" 1>>$LOG_FILE 2>&1 &
+autorestart=$(echo "$@" | grep "\-auto") 
 while true; do
 
   sleep 30
   process=$(pgrep sguclient)
   # 如果用户选择开启重连,那么sgud.sh会保活sguclient
-  if [[ $@ =~ "auto" ]]; then
+  if [ -n "$autorestart" ]; then   #不可以使用[[]],因为那是bash的扩展
     # 如果存在"sgud.sh.pid"说明并未调用stop_service或者procd守护进程并未退出，应继续保活sguclient
     if [ -z "$process" ]; then
       if [ -f "/var/run/sgud.sh.pid" ]; then
-        /bin/sguclient $@ 2&>1 1>>$LOG_FILE &
+        /bin/sguclient "$@"  1>>$LOG_FILE 2>&1 &
       fi
     fi
   else # 如果不需要重启，一旦sguclient挂掉了，也关闭sgud守护进程
